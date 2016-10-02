0b9f7881f110018db33cf458751bb01bae95e20e
talglobus <talglobus@gmail.com>
2016-10-02 01:47:01 -0400

    Make Heroku work better

--- app.js
+++ app.js
@@ -4,10 +4,12 @@
 var express = require('express');
 var app = express();
 
+const PORT = process.env.PORT || 8080;
+
 app.get('/', function (req, res) {
 	res.send('Working');
 });
 
-app.listen(8080, function () {
-	console.log('Example app listening on port 8080!');
+app.listen(PORT, function () {
+	console.log('Example app listening on port ' + PORT + '!');
 });
