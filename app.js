8de7c6afc68999fe08cd11e30281e2bf43bebbd9
talglobus <talglobus@gmail.com>
2016-10-02 02:22:43 -0400

    Changed expected location of file to seve

--- app.js
+++ app.js
@@ -2,12 +2,13 @@
  * Created by tal on 10/2/16.
  */
 var express = require('express');
+var path = require('path');
 var app = express();
 
 const PORT = process.env.PORT || 8080;
 
 app.get('/', function (req, res) {
-	res.sendFile('./index.html');
+	res.sendFile(path.join(__dirname + '/index.html'));
 	res.end();
 });
 
