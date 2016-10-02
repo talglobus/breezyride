cfe0ec072d2c16f687abf2cf0be7e9227bed6d00
talglobus <talglobus@gmail.com>
2016-10-02 10:41:23 -0400

    Fix MIME for map link so it will load instead of downloading

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ app.listen(PORT, function () {
 });
 
 function allowServeFromDir(req, res, type) {
-	var headerMIME = "image/html";     // This is a dangerous case, as it leaves html default
+	var headerMIME = "text/html";     // This is a dangerous case, as it leaves html default
 	if (type == 'jpg') {
 		headerMIME = "image/jpeg";
 	} else if (type == 'js') {
