81ea951f54d1c4e1488a28554bf9ab3831a4d250
talglobus <talglobus@gmail.com>
2016-10-02 01:41:58 -0400

    Make Heroku work

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 /**
  * Created by tal on 10/2/16.
  */
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+	res.send('Working');
+});
+
+app.listen(8080, function () {
+	console.log('Example app listening on port 8080!');
+});
