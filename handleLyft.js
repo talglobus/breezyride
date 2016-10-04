4713c45258d6edc51ca294224c1bca9f466b8c81
talglobus <talglobus@gmail.com>
2016-10-04 18:12:07 -0400

    Partial upgrade to ES6, begin logging functionality, and more

--- handleLyft.js
+++ handleLyft.js
@@ -8,13 +8,20 @@
 
 // const INC = 12;
 const INC = 30;
-const ALERT_FREQUENCY = 100;  // Note that this value is a divisor, and alerts decrease as value increases
-const DEBUGGING = false;
+const ALERT_INTERVAL = 10000;  // Note that this value is a divisor, and alerts decrease as value increases
+const DEBUGGING = !!process.env.DEBUGGING || false;
 
-var request = require('request');
-var points = require('./points');
-// const AUTH = 'Bearer gAAAAABX8A5O1EAEWuduPDVNwAEOmRstfgqpFEzJtarWlOwtfpq6SWrDTnImsF4OV6aEF5dRPvX2pC_QQHXU0frJwuZtSV0HZN4YQ7HQhrzLd3D2KKAfd_aIJ51Ypg2zzdQA1815AauoLdnJVUdpFM0qLqC7tLuabGSxVS1Av4R6K0ylRtPxQ-zHrBJOzrWnoJedwytpCHWP6yJETdfJ2kB60Mwh7CK-nQ==';
-const AUTH = 'Bearer gAAAAABX8WjU49BAEcNz3qmSH3EIiZqpwrLC-3aoGwJpGkMVgGKo3DEK4UMuIMbe67IU8EZsaMQAAQHpE6q9xwf63K7396DJ9GRc18-P4OWUoT01aDMxDuqOCvA2MdJ1nW_sJXEwObtZNFERxwIgiw22nI6Qo7eCgtOQKgrcBpb3XJdaaX8LxtM=';
+const request = require('request');
+const points = require('./points');
+// const loggerFactory = require('./logger');
+const AUTH = "Bearer " + process.env.LYFT_API_TOKEN;
+
+// const successLog = loggerFactory('./logs/successes1.txt');
+// const failLog = loggerFactory('./logs/failures1.txt');
+
+// console.log(successLog);
+// console.log(failLog);
+// console.log(loggerFactory);
 
 module.exports = theLoop;
 
@@ -43,6 +50,8 @@ function queryLocation(lat, long, cb) {
 			cb(null, data);
 		} else {
 			console.error(err);
+			debugLog(res);
+			debugLog(body);
 			cb(err, false);
 		}
 	});
@@ -55,7 +64,7 @@ function theLoop() {
 	var recentSuccessfulPings = new Array();
 	var recentFailedPings = new Array();
 
-	var seriesSent = 0; // This is the number of ALERT_FREQUENCY rounds of requests sent thus far.
+	var seriesSent = 0; // This is the number of ALERT_INTERVAL rounds of requests sent thus far.
 
 	var timeoutNum = 0;
 	console.log("Beginning Poll...");
@@ -64,41 +73,43 @@ function theLoop() {
 		var i = timeoutNum % 38;
 		// console.log("Poll " + timeoutNum + ": (" + points[i].lat + ", " + points[i].long + ")");
 		// console.log(timeoutNum + ": " + points[i]);
-		queryLocation(points[i].lat, points[i].long, function(err, res){
+		queryLocation(points[i].lat, points[i].long, (err, res) => {
 			if (!err && res) {
 				// input[i].push(res);
 				// console.log(timeoutNum + ":");
 				// var numSectionDrivers = res['nearby_drivers'][0].drivers.length;
 				// console.log(numSectionDrivers + " drivers on the road in section " + timeoutNum % 38);
 				recentSuccessfulPings.push(res);
+				// successLog.logJSON(res);
 			} else {
 				console.error("Search for X: " + points[i].x + ", Y: " + points[i].y + " failed");
 				debugLog("Error:  " + err);
 				debugLog("Result: " + res);
 				recentFailedPings.push(new Array(err, res));
+				// failLog.logJSON(new Array(err, res));
 			}
 		});
 
 
-		if (timeoutNum % ALERT_FREQUENCY == 0) {
+		if (timeoutNum % ALERT_INTERVAL == 0) {
 			seriesSent++;
 
 			var numRecentSuccessful = recentSuccessfulPings.length,
 				numRecentFailed = recentFailedPings.length,
-				totalSent = ALERT_FREQUENCY * seriesSent,
+				totalSent = ALERT_INTERVAL * seriesSent,
 				totalSuccessful = successfulPings.length + recentSuccessfulPings.length,
 				totalFailed = failedPings.length + recentFailedPings.length;
 
 			console.log(totalSent + " Pings Sent: " + totalSuccessful + " successful, " + totalFailed  + " failed, "
 				+ (totalSent - totalSuccessful - totalFailed) + " requests yet incomplete.");
 
-			console.log("Most recent " + ALERT_FREQUENCY + " Pings Sent: " + numRecentSuccessful  + " successful, "
-				+ numRecentFailed + " failed, " + (ALERT_FREQUENCY - numRecentSuccessful - numRecentFailed)
+			console.log("Most recent " + ALERT_INTERVAL + " Pings Sent: " + numRecentSuccessful  + " successful, "
+				+ numRecentFailed + " failed, " + (ALERT_INTERVAL - numRecentSuccessful - numRecentFailed)
 				+ " yet incomplete.");
 
-			console.log("Of the past " + ALERT_FREQUENCY + " pings, " + (numRecentSuccessful / ALERT_FREQUENCY * 100)
-				+ "% have been successful, " + (numRecentFailed / ALERT_FREQUENCY * 100) + "% have failed, and "
-				+ ((ALERT_FREQUENCY - numRecentSuccessful - numRecentFailed) / 100) + "% are yet incomplete.");
+			console.log("Of the past " + ALERT_INTERVAL + " pings, " + (numRecentSuccessful / ALERT_INTERVAL * 100)
+				+ "% have been successful, " + (numRecentFailed / ALERT_INTERVAL * 100) + "% have failed, and "
+				+ ((ALERT_INTERVAL - numRecentSuccessful - numRecentFailed) / ALERT_INTERVAL * 100) + "% are yet incomplete.");
 
 			successfulPings = successfulPings.concat(recentSuccessfulPings);
 			recentSuccessfulPings = [];
@@ -125,7 +136,7 @@ function theLoop() {
 
 // console.log(input);
 
-queryLocation(40.7589, -73.9851, function(err, res){
+queryLocation(40.7589, -73.9851, (err, res) => {
 	if (!err && res) {
 		debugLog(res);
 	}
---
+++ logger.js
@@ -0,0 +1,28 @@
+/**
+ * Created by tal on 10/4/16.
+ */
+
+const fs = require('fs');
+const zlib = require('zlib');
+const stream = require('stream');
+var gzip = zlib.createGzip();
+
+module.exports = (filePath) => {
+	var stream = fs.createWriteStream(filePath, {flags: "a", defaultEncoding: "utf8"});
+
+	this.logJSON = (input) => {
+		var flatInput = JSON.stringify(input);  // This could potentially be a very expensive operation
+		zlib.gzip(flatInput, (err, buffer) => {
+			if (!err) {
+				stream.write(buffer, (err, res) => {
+					if (err) {
+						stream.write(buffer, () => {}); // No error handling for file write. TODO: Fix later, seriously
+					}
+				});
+				console.log(buffer.toString('base64'));
+			} else {
+				// No error handling for gzipping, which could be an issue considering the size of the data
+			}
+		});
+	};
+};
