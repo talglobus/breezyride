12cecd86fab95b581c96fac4b1a9d3c572152a0e
talglobus <talglobus@gmail.com>
2016-10-08 19:18:32 -0400

    Remove use of Zlib to try to get Heroku to build

--- logger.js
+++ logger.js
@@ -3,7 +3,7 @@
  */
 
 const fs = require('fs');
-const zlib = require('zlib');
+// const zlib = require('zlib');
 const stream = require('stream');
 var gzip = zlib.createGzip();
 
@@ -12,17 +12,38 @@ module.exports = (filePath) => {
 
 	this.logJSON = (input) => {
 		var flatInput = JSON.stringify(input);  // This could potentially be a very expensive operation
-		zlib.gzip(flatInput, (err, buffer) => {
-			if (!err) {
-				stream.write(buffer, (err, res) => {
+		// zlib.gzip(flatInput, (err, buffer) => {
+		// 	if (!err) {
+				stream.write(flatInput, (err, res) => {
 					if (err) {
-						stream.write(buffer, () => {}); // No error handling for file write. TODO: Fix later, seriously
+						stream.write(flatInput, () => {}); // No error handling for file write. Just retrying
+						// TODO: Fix line above later, seriously
 					}
 				});
 				console.log(buffer.toString('base64'));
-			} else {
-				// No error handling for gzipping, which could be an issue considering the size of the data
-			}
-		});
+			// } else {
+			// 	// No error handling for gzipping, which could be an issue considering the size of the data
+			// }
+		// });
 	};
 };
+
+// module.exports = (filePath) => {
+// 	var stream = fs.createWriteStream(filePath, {flags: "a", defaultEncoding: "utf8"});
+//
+// 	this.logJSON = (input) => {
+// 		var flatInput = JSON.stringify(input);  // This could potentially be a very expensive operation
+// 		zlib.gzip(flatInput, (err, buffer) => {
+// 			if (!err) {
+// 				stream.write(buffer, (err, res) => {
+// 					if (err) {
+// 						stream.write(buffer, () => {}); // No error handling for file write. TODO: Fix later, seriously
+// 					}
+// 				});
+// 				console.log(buffer.toString('base64'));
+// 			} else {
+// 				// No error handling for gzipping, which could be an issue considering the size of the data
+// 			}
+// 		});
+// 	};
+// };
